43fdf872620518001905fca52f075cb195822d14
vasuquo <veasuquo@gmail.com>
2026-03-03 06:18:41 +0100

    1st commit

--- routes/static.js
+++ routes/static.js
@@ -8,6 +8,7 @@ router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
+
 module.exports = router;
 
 
--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -14,6 +15,19 @@ const static = require("./routes/static")
  * Routes
  *************************/
 app.use(static)
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+// Index Route
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})})
+
+
 
 /* ***********************
  * Local Server Information
