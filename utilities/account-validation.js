b473dd5f3cce71efaafa028f622113df1a5cd4ba
vasuquo <veasuquo@gmail.com>
2026-04-06 08:50:22 +0100

    new commit

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -114,7 +114,7 @@ const validate = {}
       .custom(async (account_email) => {
             const emailExists = await accountModel.checkExistingEmail(account_email)
             if (emailExists){
-                throw new Error("Email exists. Please log in or use different email")
+                throw new Error("Email exists. Please use different email")
             }
         }),
     ]
